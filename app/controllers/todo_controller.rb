e4d455312b0619ba522e8249eb12124db9d1dc6f
Adaliz Cruz <acruzschool18@gmail.com>
2019-03-02 19:38:44 +0000

    estimate pomodoro

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,17 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description = "make the curriculum"
-        @todo_pomodoro_estimate = 4
+        todo_id = params[:id]
+        
+        if todo_id == '1'
+        
+           @todo_description = "make the curriculum"
+           @todo_pomodoro_estimate = 4
+        
+        elsif todo_id == '2'
+        
+           @todo_description = "make the code"
+           @todo_pomodoro_estimate = 5
+        end
     end
 end
