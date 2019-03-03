14ea3233bc1b7186322c15ea957c7df92713de2b
Adaliz Cruz <acruzschool18@gmail.com>
2019-03-03 17:17:18 +0000

    edit todo

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,17 +3,9 @@ class TodoController < ApplicationController
     end
     
     def show
-        todo_id = params[:id]
+       @todo = Todo.find_by_id(params[:id])
+        # todo_id = params[:id]
         
-        if todo_id == '1'
         
-           @todo_description = "make the curriculum"
-           @todo_pomodoro_estimate = 4
-        
-        elsif todo_id == '2'
-        
-           @todo_description = "make the code"
-           @todo_pomodoro_estimate = 5
-        end
     end
 end
